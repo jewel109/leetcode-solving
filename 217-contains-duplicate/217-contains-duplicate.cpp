b3bc2c104f80d9a4333c6edf7656794b9539a27f
Raihanur Rahman <61579301+jewel109@users.noreply.github.com>
2022-07-31 10:42:45 +0600

    Time: 113 ms (82.63%), Space: 51.2 MB (69.29%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -4,6 +4,7 @@ public:
         unordered_map<int,int> umap;
 
         for(int i=0; i<nums.size(); i++){
+            // if key not found in map then add the key and return true or false
             if(umap.find(nums[i]) == umap.end()){
                 umap[nums[i]] ;
 
